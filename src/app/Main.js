414c1c8764ab41252ac73db125fd7bd1d37ce8e3
Anders Olsen Sandvik <github@andersos.net>
2016-08-11 21:17:22 +0200

    Add tabs

--- src/app/Main.js
+++ src/app/Main.js
@@ -4,6 +4,7 @@ import AppBar from 'material-ui/AppBar';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import List from 'material-ui/List';
 import MediaItem from './MediaItem.js';
+import {Tabs, Tab} from 'material-ui/Tabs';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Subheader from 'material-ui/Subheader';
 
@@ -42,47 +43,57 @@ class Main extends React.Component {
     return (
       <MuiThemeProvider muiTheme={muiTheme}>
       <div>
-      <AppBar title='Series' showMenuIconButton={false} />
+      <AppBar title='Series' onLeftIconButtonTouchTap={() => {
+          window.location.href = 'http://api.andersos.net'
+      }}/>
+      <Tabs>
+      <Tab label="Watching" >
       <List>
-      <Subheader>Watching</Subheader>
       {
         this.state.listData.filter((series) => {
+
           return watching(series);
         }).map((series) => <MediaItem series={series} key={series.name} />)
       }
       </List>
+      </Tab>
+      <Tab label="New" >
       <List>
-      <Subheader>New</Subheader>
       {
         this.state.listData.filter((series) => {
           return newSeries(series);
         }).map((series) => <MediaItem series={series} key={series.name} />)
       }
       </List>
+      </Tab>
+      <Tab label="End of season" >
       <List>
-      <Subheader>End of season</Subheader>
       {
         this.state.listData.filter((series) => {
           return endOfSeason(series);
         }).map((series) => <MediaItem series={series} key={series.name} />)
       }
       </List>
+      </Tab>
+      <Tab label="Ignored" >
       <List>
-      <Subheader>Ignored</Subheader>
       {
         this.state.listData.filter((series) => {
           return ignored(series);
         }).map((series) => <MediaItem series={series} key={series.name} />)
       }
       </List>
+      </Tab>
+      <Tab label="Done" >
       <List>
-      <Subheader>Done</Subheader>
       {
         this.state.listData.filter((series) => {
           return done(series);
         }).map((series) => <MediaItem series={series} key={series.name} />)
       }
       </List>
+      </Tab>
+      </Tabs>
       </div>
       </MuiThemeProvider>
     );
